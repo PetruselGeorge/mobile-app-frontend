9b72eb1c07b2f9774ff77e0fc42e417cee207b7c
petru <george.petrusel@stud.ubbcluj.ro>
2023-05-09 16:37:17 +0300

    day1

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {Button, View} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import TrailListScreen from "./src/screens/TrailListScreen";
+import TrailDetailScreen from "./src/screens/TrailDetailScreen";
+import {createStackNavigator} from "@react-navigation/stack";
+import TrailCreateScreen from "./src/screens/TrailCreateScreen";
+import AccountScreen from "./src/screens/AccountScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import SigninScreen from "./src/screens/SigninScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function HomeScreen({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Button
+                onPress={() => navigation.navigate('Notifications')}
+                title="Go to notifications"
+            />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function NotificationsScreen({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Button onPress={() => navigation.goBack()} title="Go back home"/>
+        </View>
+    );
+}
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name={"TrailListScreen"} component={TrailListScreen}/>
+            <Stack.Screen name={"TrailDetailScreen"} component={TrailDetailScreen}/>
+        </Stack.Navigator>
+    )
+}
+
+const DrawerPart = () => {
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name={"stackNavigator"} component={StackNavigator}/>
+            <Drawer.Screen name={"TrailCreateScreen"} component={TrailCreateScreen}/>
+            <Drawer.Screen name={"AccountScreen"} component={AccountScreen}/>
+        </Drawer.Navigator>
+    )
+}
+
+export default () => {
+    return (<>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name={"SignupScreen"} component={SignupScreen}/>
+                    <Stack.Screen name={"SigninScreen"} component={SigninScreen}/>
+                    <Stack.Screen name={"drawerPart"} component={DrawerPart}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+
+        </>
+    );
+}
